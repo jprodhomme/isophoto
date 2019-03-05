642cd4b97a4e81e8ebffabce63cbca345b15b1b8
jprodhomme <prodhomme.julien@gmail.com>
2019-03-05 15:19:47 +0100

    first push

--- src/main/java/co/simplon/titrepro/isophoto/IsophotoApplication.java
+++ src/main/java/co/simplon/titrepro/isophoto/IsophotoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IsophotoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IsophotoApplication.class, args);
+		SpringApplication.run(IsophotoApplication.class, args); //test
 	}
 
 }
