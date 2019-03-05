ead585a5e5dfc2c2350f152f36063e7437f4dd8a
jprodhomme <prodhomme.julien@gmail.com>
2019-03-05 15:17:00 +0100

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# isophoto
-Projet Titre Pro
---
+++ src/main/java/co/simplon/titrepro/isophoto/IsophotoApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.titrepro.isophoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsophotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsophotoApplication.class, args);
+	}
+
+}
