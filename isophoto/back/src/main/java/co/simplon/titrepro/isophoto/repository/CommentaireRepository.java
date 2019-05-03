5f2475835ab2c0cfbf2edc8df802b4cce0000466
jprodhomme <prodhomme.julien@gmail.com>
2019-05-03 13:23:45 +0200

    remove isophoto

--- isophoto/back/src/main/java/co/simplon/titrepro/isophoto/repository/CommentaireRepository.java
+++
@@ -1,9 +0,0 @@
-package co.simplon.titrepro.isophoto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.simplon.titrepro.isophoto.model.Commentaire;
-
-public interface CommentaireRepository extends JpaRepository<Commentaire, Long> {
-
-}
--- isophoto/back/src/main/java/co/simplon/titrepro/isophoto/services/CommentaireService.java
+++
@@ -1,9 +0,0 @@
-package co.simplon.titrepro.isophoto.services;
-
-import co.simplon.titrepro.isophoto.model.Photo;
-
-public interface CommentaireService {
-
-	Photo addCommentaire(long idPhoto, String commentaireString);
-
-}
